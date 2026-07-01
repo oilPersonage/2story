73ba5fa007930574f8d92fbfd130e6e33c080c62
oilPersonage <oilpersonage@gmail.com>
2026-07-01 10:18:34 +0800

    fix styles icons

--- src/copy.js
+++ src/copy.js
@@ -1,19 +1,17 @@
 import { animate } from "animejs";
 
 const items = [...document.querySelectorAll("[data-for-copy]")];
-const notify = document.querySelector(".notification");
 
 items.forEach((el) => {
   el.addEventListener("click", async (e) => {
+    const copyText = el.querySelector(".notification-text");
     e.preventDefault();
-    const { top, left, height } = e.target.getBoundingClientRect();
     const { forCopy } = e.target.dataset;
     await navigator.clipboard.writeText(forCopy);
-    notify.style.left = left + "px";
-    animate(notify, {
-      y: [
-        { from: top - height, duration: 300, to: "-=10" },
-        { to: "+=10", delay: 1000 },
+    animate(copyText, {
+      x: [
+        { from: 0, duration: 300, to: "+=10" },
+        { to: "-=10", delay: 1000 },
       ],
       opacity: [
         { from: 0, duration: 300, to: 1 },
